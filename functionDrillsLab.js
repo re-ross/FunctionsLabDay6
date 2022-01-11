522ce286897b0876de53acd09c7d1732af2e91cf
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-10 18:11:29 -0600

    solved both gold & frog problems

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -332,14 +332,26 @@ console.log(emailCheck(" ryan@email.com  "));
 */
 
 //CODE HERE
+function trolleyForRon(gold) {
+  let frogResult = gold / 3;
+  return frogResult;
+}
 
+totalFrogs = trolleyForRon(210);
+console.log(totalFrogs);
 ////////////////// PROBLEM 20 ////////////////////
 /*
   You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
 */
 
 //CODE HERE
+function trolleyForRon2(gold) {
+  let frogResult = gold / 3;
+  return Math.floor(frogResult);
+}
 
+totalFrogs2 = trolleyForRon2(4);
+console.log(totalFrogs2);
 ////////////////// PROBLEM 21 ////////////////////
 let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9];
 /*
