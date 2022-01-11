82f2f5090902d76eac9cfc01956d715aa3d212f0
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-10 20:41:31 -0600

    finished lab

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -357,9 +357,24 @@ let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9];
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
-
 //CODE HERE
+function ascendingArray(arr) {
+  let value = arr[0];
+  for (i = 0; i < arr.length - 1; i++) {
+    if (arr[i] <= value) {
+      // console.log(arr[i], value);
+      return false;
+    } else {
+      // console.log(arr[i], value);
+      value = arr[i];
+      // console.log(arr[i], value);
+    }
+  }
+  return true;
+}
 
+let arrayIsAscending = ascendingArray(sampleArray);
+console.log(arrayIsAscending);
 ////////////////// PROBLEM 22 ////////////////////
 
 let duck = "cute";
@@ -382,13 +397,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = [];
+let globalScope = [duck];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = [];
+let bathroomScope = [rubberDuck];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = [];
+let bathtubScope = [sailorDuck];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = [];
+let pondScope = [realDuck];
